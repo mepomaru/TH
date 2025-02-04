8a5d040d4468098118542a912a1dc8eba692cf6a
mepomaru <2230223@ecc.ac.jp>
2025-02-04 11:45:48 +0900

    add define questions

--- data/def/def.php
+++ data/def/def.php
@@ -12,6 +12,5 @@ define("DB_NAME", "trueHue");
 define("DB_CHARSET", "utf8mb4");
 
 define('TBL_USER', "USER");
-define('TBL_ANS', "ANSWER");
-define('TBL_SBJ_QES', "SUBJECTIVE_QES");
-define('TBL_OBJ_QES', 'OBJECTIVE_QES');
+define('TBL_QES', "QUESTIONS");
+define('TBL_ANS', "ANSWERS");
