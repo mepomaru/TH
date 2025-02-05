259c7580e0a7ea3f213626792c0411e45f999968
mepomaru <2230223@ecc.ac.jp>
2025-02-05 12:05:49 +0900

    change page title

--- public_html/home/php/remind.php
+++ public_html/home/php/remind.php
@@ -44,7 +44,7 @@ $_SESSION['uname'] = $user['uname'];
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>TrueHue - ご使用上の注意</title>
+    <title>ご使用上の注意</title>
     <link rel="stylesheet" href="../css/remind.css">
 </head>
 
